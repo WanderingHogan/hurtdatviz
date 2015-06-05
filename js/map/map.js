d83441270a45da415fad635d1861f65590635a01
Chris Hogan <chris@hogan.io>
2015-06-05 02:45:48 -0400

    adding quick basemap, need to sign up for mapbox for tiles/switcher

--- js/map/map.js
+++ js/map/map.js
@@ -1,48 +1,41 @@
 var mapStuff = angular.module('mapStuff',['ngRoute']);
 
-mapStuff.controller('mapController', ['$scope', 'stormData', 
+mapStuff.controller('mapController', ['$scope', 'stormData',
 	function($scope, stormData){
-		console.log('hey');
 
+			// default baltimore starts
+		    var startX = 39.290452,
+		        startY = -76.614090,
+		        startZ = 13;
+
+
+		    var map = L.map('map',{
+		    	'inertia': false,
+		        'minZoom': 11,
+		        'maxZoom': 6,
+		        'zoomControl': false, 
+		        'scrollWheelZoom': false
+		    }).setView([startX, startY], startZ);
+
+		    this.basemap = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		        'maxZoom': 18,
+		        'attribution': '© mapbox',
+		        'id': 'examples.map-20v6611k'
+		    }).addTo(map);
+
+		    this.satellite = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		        'maxZoom': 18,
+		        'attribution': '© mapbox',
+		        'id': 'mapbox.streets'
+		    }).addTo(map);
 	//
 }]);
 
-'use strict';
+mapStuff.directive('map', function() {
+	return {
+		templateUrl: 'js/layout/partials/map.tpl.html',
+		restrict: 'E',
+		controller: 'mapController'
 
-mapStuff.directive('map', ['$rootScope', '$scope',
-	function($rootScope, $scope) {
-		
-		return {
-			templateUrl: 'js/layout/partials/map.tpl.html',
-			restrict: 'E',
-			controller: 'mapController',
-			link: function (scope, element, attrs) {
-				// default baltimore starts
-			    var startX = 39.290452,
-			        startY = -76.614090,
-			        startZ = 13,
-			        tractsQueue = [],
-			        isMapMoving = false;
-
-			    var tractLayer,
-			        geocodedMarkers = [];
-
-			    var map = L.map('map',{
-			    	'inertia': false,
-			        'minZoom': 11,
-			        'maxZoom': 16,
-			        'zoomControl': false, 
-			        'scrollWheelZoom': false
-			    }).setView([startX, startY], startZ);
-
-			    window.basemap = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-			        'maxZoom': 18,
-			        'attribution': '© mapbox',
-			        'id': 'examples.map-20v6611k'
-			    }).addTo(map);
-
-		    }
-
-		};
-	}
-]);
+	};
+});
